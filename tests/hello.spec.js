5bc1952e3d545ee9be321ed2416677a825b319ab
Yair Rodriguez <rodriguezortega.yair@gmail.com>
2016-12-13 22:11:24 -0600

    [files] Remove default files.

--- tests/hello.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import sayHello from '../src/hello';
-
-describe('Hello World', () => {
-  it('should work', () => {
-    expect(sayHello('Jair')).to.equal('Hello Jair');
-  });
-});
